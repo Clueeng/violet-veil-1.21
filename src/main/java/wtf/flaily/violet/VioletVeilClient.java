5e0369800e7264bb4ce299fae64a40623d05fd22
flaily <clueeng@gmail.com>
2025-04-23 13:10:23 +0200

    Initial commit

---
+++ src/main/java/wtf/flaily/violet/VioletVeilClient.java
@@ -0,0 +1,10 @@
+package wtf.flaily.violet;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VioletVeilClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
